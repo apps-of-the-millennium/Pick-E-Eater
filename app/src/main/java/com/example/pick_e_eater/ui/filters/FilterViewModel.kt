28ce44b594be06549d50dfcc72dfefa97255a87d
galegoer <eric.m.galego@gmail.com>
2023-07-13 03:15:20 -0400

    completed filters page

--- app/src/main/java/com/example/pick_e_eater/ui/filters/FilterViewModel.kt
+++ app/src/main/java/com/example/pick_e_eater/ui/filters/FilterViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.pick_e_eater.ui.dashboard
+package com.example.pick_e_eater.ui.filters
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class FilterViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is a filters Fragment"
     }
     val text: LiveData<String> = _text
 }
